df9334000ae7feefec9643f068830628faaf071b
mmahdavis <mmahdavis20@gmail.com>
2023-04-30 16:40:57 +0430

    app.blade.php changed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
 
         <!-- Scripts -->
         @routes
-        @vite(['resources/css/styles.scss','resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
     <body class="font-sans antialiased ">
