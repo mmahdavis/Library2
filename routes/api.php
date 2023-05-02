85199468171ae250f750a64e089dda6b23be1c9e
mmahdavis <mmahdavis20@gmail.com>
2023-05-02 17:11:30 +0430

    Fix Duplicate Route Name

--- routes/api.php
+++ routes/api.php
@@ -32,42 +32,84 @@ Route::middleware([
         'except' => [
             'create',
             'edit'
-        ]
+        ],
+        'names' => array(
+            'index' => 'api.books.index',
+            'store' => 'api.books.store',
+            'show' => 'api.books.show',
+            'update' => 'api.books.update',
+            'destroy' => 'api.books.destroy',
+        )
     ]);
 
     Route::resource('writers', WriterController::class, [
         'except' => [
             'create',
             'edit'
-        ]
+        ],
+        'names' => array(
+            'index' => 'api.writers.index',
+            'store' => 'api.writers.store',
+            'show' => 'api.writers.show',
+            'update' => 'api.writers.update',
+            'destroy' => 'api.writers.destroy',
+        )
     ]);
 
     Route::resource('publishers', PublisherController::class, [
         'except' => [
             'create',
             'edit'
-        ]
+        ],
+        'names' => array(
+            'index' => 'api.publishers.index',
+            'store' => 'api.publishers.store',
+            'show' => 'api.publishers.show',
+            'update' => 'api.publishers.update',
+            'destroy' => 'api.publishers.destroy',
+        )
     ]);
 
     Route::resource('translators', TranslatorController::class, [
         'except' => [
             'create',
             'edit'
-        ]
+        ],
+        'names' => array(
+            'index' => 'api.translators.index',
+            'store' => 'api.translators.store',
+            'show' => 'api.translators.show',
+            'update' => 'api.translators.update',
+            'destroy' => 'api.translators.destroy',
+        )
     ]);
 
     Route::resource('magazines', MagazineController::class, [
         'except' => [
             'create',
             'edit'
-        ]
+        ],
+        'names' => array(
+            'index' => 'api.magazines.index',
+            'store' => 'api.magazines.store',
+            'show' => 'api.magazines.show',
+            'update' => 'api.magazines.update',
+            'destroy' => 'api.magazines.destroy',
+        )
     ]);
 
     Route::resource('special_issues', SpecialIssueController::class, [
         'except' => [
             'create',
             'edit'
-        ]
+        ],
+        'names' => array(
+            'index' => 'api.special_issues.index',
+            'store' => 'api.special_issues.store',
+            'show' => 'api.special_issues.show',
+            'update' => 'api.special_issues.update',
+            'destroy' => 'api.special_issues.destroy',
+        )
     ]);
 
 });
